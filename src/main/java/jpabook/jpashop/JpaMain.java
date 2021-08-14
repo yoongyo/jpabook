a0192c421fad7b4bbfd7731fe69cbb049b5de686
unknown <jyg0172@naver.com>
2021-08-15 01:43:04 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        // code
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        em.clear();
+
+        emf.close();
+    }
+}
